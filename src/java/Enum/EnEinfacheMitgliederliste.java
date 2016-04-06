cd9e1b6e5c07a0f4d7bc345e62f3239d213feae3
Nauschi <philipp.n97@live.at>
2016-04-06 08:51:38 +0200

    EinfacheMitgliederListe Where Clause

--- src/java/Enum/EnEinfacheMitgliederliste.java
+++ src/java/Enum/EnEinfacheMitgliederliste.java
@@ -9,31 +9,30 @@ package Enum;
  *
  * @author Corinna
  */
-public enum EnEinfacheMitgliederliste
-{
-    
+public enum EnEinfacheMitgliederliste {
+
     getEinfacheMitgliederlisteBereich("SELECT id_personen 'PersID', standesbuchnummer 'STB', dienstgrad 'DGR', titel 'Titel', vorname 'Vorname', zuname 'Zuname' "
-                        + " FROM FDISK.dbo.stmkmitglieder s INNER JOIN FDISK.dbo.qry_alle_feuerwehren_mit_Abschnitt_und_Bereich f ON(s.instanznummer = f.instanznummer) "
-                        + " WHERE f.Bereich_Nr = ?"),
+            + " FROM FDISK.dbo.stmkmitglieder s INNER JOIN FDISK.dbo.qry_alle_feuerwehren_mit_Abschnitt_und_Bereich f ON(s.instanznummer = f.instanznummer) "
+            + " WHERE (FDISK.dbo.stmkmitglieder.abgemeldet = 0) AND (NOT (LEFT(FDISK.dbo.stmkmitglieder.instanznummer, 2) = 'GA')) AND (NOT (LEFT(FDISK.dbo.stmkmitglieder.instanzname, 7) = 'FW GAST'))"
+            + " AND f.Bereich_Nr = ?"),
     getEinfacheMitgliederlisteAbschnitt("SELECT id_personen 'PersID', standesbuchnummer 'STB', dienstgrad 'DGR', titel 'Titel', vorname 'Vorname', zuname 'Zuname' "
-                        + " FROM FDISK.dbo.stmkmitglieder s INNER JOIN FDISK.dbo.qry_alle_feuerwehren_mit_Abschnitt_und_Bereich f ON(s.instanznummer = f.instanznummer) "
-                        + " WHERE f.abschnitt_instanznummer = ?"),
+            + " FROM FDISK.dbo.stmkmitglieder s INNER JOIN FDISK.dbo.qry_alle_feuerwehren_mit_Abschnitt_und_Bereich f ON(s.instanznummer = f.instanznummer) "
+            + " WHERE (FDISK.dbo.stmkmitglieder.abgemeldet = 0) AND (NOT (LEFT(FDISK.dbo.stmkmitglieder.instanznummer, 2) = 'GA')) AND (NOT (LEFT(FDISK.dbo.stmkmitglieder.instanzname, 7) = 'FW GAST'))"
+            + " AND f.abschnitt_instanznummer = ?"),
     getEinfacheMitgliederlisteFubwehr("SELECT id_personen 'PersID', standesbuchnummer 'STB', dienstgrad 'DGR', titel 'Titel', vorname 'Vorname', zuname 'Zuname' "
-                        + "FROM FDISK.dbo.stmkmitglieder "
-                        + "WHERE instanznummer = ?");
-    
+            + " FROM FDISK.dbo.stmkmitglieder "
+            + " WHERE (FDISK.dbo.stmkmitglieder.abgemeldet = 0) AND (NOT (LEFT(FDISK.dbo.stmkmitglieder.instanznummer, 2) = 'GA')) AND (NOT (LEFT(FDISK.dbo.stmkmitglieder.instanzname, 7) = 'FW GAST'))"
+            + " AND instanznummer = ?");
+
     private final String strStatement;
 
-    private EnEinfacheMitgliederliste(String strStatement)
-    {
+    private EnEinfacheMitgliederliste(String strStatement) {
         this.strStatement = strStatement;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return strStatement;
     }
-    
-    
+
 }
